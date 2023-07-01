01f81236bc4c998fdeb36070f2106434ae6f01ed
bearddan2000 <bearddan2000@yahoo.com>
2023-07-01 08:25:35 -0400

    initial commit

---
+++ reactjs-srv/bin/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Datarow from './component/Datarow';
+
+function App() {
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const login = btoa("maria:pass");
+    axios.get('https://localhost/', {
+      headers: {
+        'Authorization': `Basic ${login}`
+      }
+    })
+    .then(response => setUsers(response.data))
+  }, []);
+
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <h1>Promise axios api with basicAuth and ssl json</h1>
+        {users.map((user, index) =>
+        (
+         <Datarow dataFieldId={user.id} dataFieldBreed={user.breed} 
+          dataFieldColor={user.color}/>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
